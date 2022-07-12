d7417b9a5849bb216017caa0303d8a26c07a92f3
natsumesou <natsumesouxx@gmail.com>
2022-07-12 19:47:57 +0900

    fix text

--- src/components/organisms/Title.tsx
+++ src/components/organisms/Title.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Title = () => {
   return (
     <>
-      <p className="text-white text-2xl font-bold">世界を加速させる。</p>
+      <p className="text-white text-2xl font-bold">世界を加速させる</p>
       <h1 className="text-white text-6xl font-bold">vonxai inc.</h1>
     </>
   )
