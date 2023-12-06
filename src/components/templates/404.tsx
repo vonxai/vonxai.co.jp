002f665078e2b984a6cacf264cd64fcff905dc75
natsumesou <natsumesouxx@gmail.com>
2023-12-06 19:05:48 +0900

    404の修正

--- src/components/templates/404.tsx
+++ src/components/templates/404.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import Base from "./Base";
 
 const NotFound = ({ pageContext }) => {
-  return <Base pageContext={pageContext}>404 Not Found.</Base>;
+  return (
+    <Base pageContext={pageContext}>
+      <p className="text-center">404 Not Found.</p>
+    </Base>
+  );
 };
 
 export default NotFound;
