350e05e3e6007ef4776effbfff726bac9c120dcf
natsumesou <natsumesouxx@gmail.com>
2024-01-31 01:16:53 +0900

    テキスト修正

--- src/components/organisms/Title.tsx
+++ src/components/organisms/Title.tsx
@@ -4,7 +4,7 @@ export const Title = () => {
   return (
     <div className="py-5 mx-auto max-w-2xl w-42rem px-2">
       <p className="text-white text-2xl md:my-1 font-bold auto-phrase">
-        タイパを求める時代に、技術と向き合う
+        タイパを求める時代に、技術と向き合う。
       </p>
       <h1 className="text-white text-8xl md:text-6xl font-bold">vonxai inc.</h1>
     </div>
