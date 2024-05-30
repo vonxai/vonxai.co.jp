1ae449e34c471de988a41b403034351c55ed6a39
natsumesou <natsumesouxx@gmail.com>
2024-05-30 22:26:47 +0900

    delete hard tab

--- src/components/atoms/BlockWithHeader.tsx
+++ src/components/atoms/BlockWithHeader.tsx
@@ -11,7 +11,7 @@ export const BlockWithHeader: React.FC<{
     Tag === "h2" ? "text-4xl md:text-2xl" : "text-2xl md:text-xl";
   const border =
     Tag === "h2"
-      ? "before:content-[''] before:block before:absolute before:left-0	before:bottom-0 before:w-full	before:h-px	before:bg-gray-200 after:content-[''] after:block after:absolute after:left-0	after:bottom-0 after:w-1/3	after:h-px	after:bg-gray-500"
+      ? "before:content-[''] before:block before:absolute before:left-0 before:bottom-0 before:w-full before:h-px before:bg-gray-200 after:content-[''] after:block after:absolute after:left-0 after:bottom-0 after:w-1/3 after:h-px after:bg-gray-500"
       : "";
   const bottomSpace = Tag === "h2" ? "pb-7" : "pb-5";
   const titleEl =
